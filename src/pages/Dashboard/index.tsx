7f6d42f8caf1b43b037a442273458e2fa9f36a15
heresson <heresson@gmail.com>
2020-05-15 14:17:00 -0300

    navegando entre rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, {useState, FormEvent, useEffect} from 'react';
+import { Link } from 'react-router-dom'
 import {FiChevronRight} from  'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -71,7 +72,7 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
 		{repositories.map(repository => (
-				<a key={repository.full_name} href="teste">
+				<Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
 					<img
 						src={repository.owner.avatar_url}
 						alt={repository.owner.login}
@@ -82,7 +83,7 @@ const Dashboard: React.FC = () => {
 						<p>{repository.description}</p>
 					</div>
 					<FiChevronRight size={20} />
-				</a>
+        </Link>
 		))}
 	</Repositories>
     </>
