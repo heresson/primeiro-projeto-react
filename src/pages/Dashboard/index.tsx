e0bd57a0f089a994e68db39df59cda8b4835b29c
heresson <heresson@gmail.com>
2020-05-15 12:56:47 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react'
-import {Title} from './styles';
+import {FiChevronRight} from  'react-icons/fi';
 
+import logoImg from '../../assets/logo.svg';
+import {Title, Form, Repositories} from './styles';
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no GitHub</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositorios no GitHub</Title>
+
+      <Form>
+				<input placeholder="Digite o nome do repositorio" />
+				<button type="submit">Pesquisar</button>
+			</Form>
+
+      <Repositories>
+      <a href="teste">
+					<img
+						src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
+						alt="Heresson Mendes"
+					/>
+
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Easy peasy highly scalable ReactJS and React Native forms</p>
+					</div>
+        <FiChevronRight size={20} />
+
+				</a>
+        <a href="teste">
+					<img
+						src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
+						alt="Heresson Mendes"
+					/>
+
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Easy peasy highly scalable ReactJS and React Native forms</p>
+					</div>
+        <FiChevronRight size={20} />
+
+				</a>
+        <a href="teste">
+					<img
+						src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
+						alt="Heresson Mendes"
+					/>
+
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Easy peasy highly scalable ReactJS and React Native forms</p>
+					</div>
+        <FiChevronRight size={20} />
+
+				</a>
+      </Repositories>
+    </>
+  );
 }
 export default Dashboard;
