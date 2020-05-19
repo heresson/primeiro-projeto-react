d337906879c13d564a522a65ce474ab2b3891d9f
heresson <heresson@gmail.com>
2020-05-19 11:48:59 -0300

    concluido

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,43 +1,49 @@
-import React, {useState, FormEvent, useEffect} from 'react';
-import { Link } from 'react-router-dom'
-import {FiChevronRight} from  'react-icons/fi';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
-import {Title, Form, Repositories, Error} from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 import api from '../../services/api';
 
 interface Repository {
   full_name: string;
   description: string;
-	owner: {
-		login: string;
-		avatar_url:string;
-	}
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
 }
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState(''); //armazenar o valor do input
   const [inputError, setInputError] = useState('');
 
-  const [repositories, setRepositories] = useState<Repository[]> ( () => {
-    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
 
-    if(storagedRepositories) {
+    if (storagedRepositories) {
       return JSON.parse(storagedRepositories);
     } else {
       return [];
     }
   });
 
-  useEffect( () => {
-    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
-  }, [repositories] );
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
-
-  async function handleAddRepository(event: FormEvent<HTMLFormElement>) : Promise<void> {
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
     event.preventDefault();
-    if(!newRepo) {
+    if (!newRepo) {
       setInputError('Digite autor/nome do respositorio');
       return;
     }
@@ -52,9 +58,7 @@ const Dashboard: React.FC = () => {
     } catch (Err) {
       setInputError('Erro na busca por esse repositorio');
     }
-
-
-	}
+  }
 
   return (
     <>
@@ -64,29 +68,32 @@ const Dashboard: React.FC = () => {
       <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
-          onChange={ (e) => setNewRepo(e.target.value)}
+          onChange={(e) => setNewRepo(e.target.value)}
           placeholder="Digite o nome do repositorio"
         />
-				<button type="submit">Pesquisar</button>
-			</Form>
+        <button type="submit">Pesquisar</button>
+      </Form>
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
-		{repositories.map(repository => (
-				<Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
-					<img
-						src={repository.owner.avatar_url}
-						alt={repository.owner.login}
-					/>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-					<div>
-						<strong>{repository.full_name}</strong>
-						<p>{repository.description}</p>
-					</div>
-					<FiChevronRight size={20} />
-        </Link>
-		))}
-	</Repositories>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
     </>
   );
-}
+};
 export default Dashboard;
